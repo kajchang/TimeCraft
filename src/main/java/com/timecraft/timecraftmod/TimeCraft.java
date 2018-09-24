c40b0309312690f9a5c788ac03b0e08e4c6a8160
Kai Chang <kaijchang@gmail.com>
2018-09-23 23:04:34 -0700

    basic logging of clicks

--- src/main/java/com/timecraft/timecraftmod/TimeCraft.java
+++ src/main/java/com/timecraft/timecraftmod/TimeCraft.java
@@ -1,15 +1,44 @@
 package com.timecraft.timecraftmod;
 
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import net.minecraft.util.EnumHand;
 
 @Mod(modid = TimeCraft.MODID, version = TimeCraft.VERSION, useMetadata = true)
 public class TimeCraft {
     public static final String MODID = "timecraft";
     public static final String VERSION = "0.1.0";
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
-        System.out.println("Starting Forge");
+    @Mod.EventBusSubscriber(modid = TimeCraft.MODID)
+    public static class TimeCraftEventHandler {
+        @SubscribeEvent
+        public static void onLoad(WorldEvent.Load event) {
+            System.out.println("Entering " + event.getWorld());
+        }
+
+        @SubscribeEvent
+        public static void onLeftClick(PlayerInteractEvent.LeftClickBlock event) {
+            EnumHand hand = event.getHand();
+
+            if (hand.name().equals("MAIN_HAND")) {
+                System.out.println("You've hit using " + event.getEntityPlayer().getHeldItemMainhand().getDisplayName());
+            } else {
+                System.out.println("You've hit using " + event.getEntityPlayer().getHeldItemOffhand().getDisplayName());
+            }
+        }
+
+        @SubscribeEvent
+        public static void onRightClick(PlayerInteractEvent.RightClickBlock event) {
+            EnumHand hand = event.getHand();
+
+            if (hand.name().equals("MAIN_HAND")) {
+                System.out.println("You've interacted using " + event.getEntityPlayer().getHeldItemMainhand().getDisplayName());
+            } else {
+                System.out.println("You've interacted using " + event.getEntityPlayer().getHeldItemOffhand().getDisplayName());
+            }
+        }
     }
 }
